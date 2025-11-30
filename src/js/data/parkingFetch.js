5bbac217dfca4fc8cd01d760ebbfaca165364686
PowerelectricGI <kkh1876@gmail.com>
2025-11-30 22:54:22 +0900

    R15

--- src/js/data/parkingFetch.js
+++ src/js/data/parkingFetch.js
@@ -86,6 +86,11 @@ export class ParkingDataFetcher {
      * @param {string} html - HTML 문자열
      * @returns {Object} 단기주차장 데이터
      */
+    /**
+     * 단기주차장 HTML 파싱 (Label Matching)
+     * @param {string} html - HTML 문자열
+     * @returns {Object} 단기주차장 데이터
+     */
     static parseShortTermHTML(html) {
         const parser = new DOMParser();
         const doc = parser.parseFromString(html, 'text/html');
@@ -97,55 +102,25 @@ export class ParkingDataFetcher {
         };
 
         try {
-            // CSS 선택자로 변환 (XPath 대응)
-            // #menu964_obj1181 > div > div:nth-child(4) > ul > li:nth-child(N) > div > div:nth-child(2) > div:first-child
-            const baseSelector = '#menu964_obj1181';
+            const container = doc.querySelector('#menu964_obj1181');
+            if (!container) throw new Error('Short term container not found');
 
-            // 지상 1층
-            const floor1Text = this.getTextBySelector(doc,
-                `${baseSelector} > div > div:nth-child(4) > ul > li:nth-child(1) > div > div:nth-child(2) > div:first-child`
-            ) || this.getTextBySelector(doc,
-                `${baseSelector} div > div:nth-of-type(4) ul li:nth-child(1) div div:nth-of-type(2) div:first-child`
-            );
-            result.floor1.available = this.extractNumber(floor1Text);
+            const items = container.querySelectorAll('ul > li');
+            items.forEach(item => {
+                const labelEl = item.querySelector('.num-txt');
+                const valEl = item.querySelector('.num-noti strong');
 
-            // 지하 1층
-            const basement1Text = this.getTextBySelector(doc,
-                `${baseSelector} > div > div:nth-child(4) > ul > li:nth-child(2) > div > div:nth-child(2) > div:first-child`
-            ) || this.getTextBySelector(doc,
-                `${baseSelector} div > div:nth-of-type(4) ul li:nth-child(2) div div:nth-of-type(2) div:first-child`
-            );
-            result.basement1.available = this.extractNumber(basement1Text);
+                if (labelEl && valEl) {
+                    const label = labelEl.textContent.trim();
+                    const val = this.extractNumber(valEl.textContent);
 
-            // 지하 2층
-            const basement2Text = this.getTextBySelector(doc,
-                `${baseSelector} > div > div:nth-child(4) > ul > li:nth-child(3) > div > div:nth-child(2) > div:first-child`
-            ) || this.getTextBySelector(doc,
-                `${baseSelector} div > div:nth-of-type(4) ul li:nth-child(3) div div:nth-of-type(2) div:first-child`
-            );
-            result.basement2.available = this.extractNumber(basement2Text);
-
-            // 대체 방법: li 요소들을 순회하며 찾기
-            if (result.floor1.available === 0 && result.basement1.available === 0) {
-                const container = doc.querySelector('#menu964_obj1181');
-                if (container) {
-                    const listItems = container.querySelectorAll('ul li');
-                    listItems.forEach((li, index) => {
-                        // div > div:nth-child(2) > div:first-child 에서 숫자 찾기
-                        const numDiv = li.querySelector('div > div:nth-child(2) > div:first-child') ||
-                            li.querySelector('div div:nth-of-type(2) div:first-child');
-                        if (numDiv) {
-                            const num = this.extractNumber(numDiv.textContent);
-                            if (index === 0) result.floor1.available = num;
-                            else if (index === 1) result.basement1.available = num;
-                            else if (index === 2) result.basement2.available = num;
-                        }
-                    });
+                    if (label.includes('지상 1층')) result.floor1.available = val;
+                    else if (label.includes('지하 1층')) result.basement1.available = val;
+                    else if (label.includes('지하 2층')) result.basement2.available = val;
                 }
-            }
+            });
 
             console.log('단기주차장 파싱 결과:', result);
-
         } catch (error) {
             console.error('단기주차장 파싱 오류:', error);
         }
@@ -154,17 +129,7 @@ export class ParkingDataFetcher {
     }
 
     /**
-     * 장기주차장 HTML 파싱
-     * XPath 기반:
-     * 동편 (div[4]):
-     * - 장기주차장 P1: //*[@id="menu965_obj1182"]/div/div[4]/ul/li[1]/div/div[2]/div[1]
-     * - 주차타워 동편: //*[@id="menu965_obj1182"]/div/div[4]/ul/li[2]/div/div[2]/div[1]
-     * - 장기주차장 P3: //*[@id="menu965_obj1182"]/div/div[4]/ul/li[3]/div/div[2]/div[1]
-     * 서편 (div[5]):
-     * - 장기주차 P2: //*[@id="menu965_obj1182"]/div/div[5]/ul/li[1]/div/div[2]/div[1]
-     * - 주차타워 서편: //*[@id="menu965_obj1182"]/div/div[5]/ul/li[2]/div/div[2]/div[1]
-     * - 장기주차 P4: //*[@id="menu965_obj1182"]/div/div[5]/ul/li[3]/div/div[2]/div[1]
-     * 
+     * 장기주차장 HTML 파싱 (Label Matching)
      * @param {string} html - HTML 문자열
      * @returns {Object} 장기주차장 데이터
      */
@@ -186,80 +151,50 @@ export class ParkingDataFetcher {
         };
 
         try {
-            const baseSelector = '#menu965_obj1182';
+            const container = doc.querySelector('#menu965_obj1182');
+            if (!container) throw new Error('Long term container not found');
 
-            // 동편 (div:nth-child(4))
-            // 장기주차장 P1
-            const p1Text = this.getTextBySelector(doc,
-                `${baseSelector} > div > div:nth-child(4) > ul > li:nth-child(1) > div > div:nth-child(2) > div:first-child`
-            );
-            result.east.p1.available = this.extractNumber(p1Text);
+            // Find all ULs to distinguish East vs West sections
+            const uls = container.querySelectorAll('ul');
 
-            // 주차타워 동편
-            const towerEastText = this.getTextBySelector(doc,
-                `${baseSelector} > div > div:nth-child(4) > ul > li:nth-child(2) > div > div:nth-child(2) > div:first-child`
-            );
-            result.east.tower.available = this.extractNumber(towerEastText);
+            uls.forEach(ul => {
+                const items = ul.querySelectorAll('li');
+                let isEast = false;
+                let isWest = false;
 
-            // 장기주차장 P3
-            const p3Text = this.getTextBySelector(doc,
-                `${baseSelector} > div > div:nth-child(4) > ul > li:nth-child(3) > div > div:nth-child(2) > div:first-child`
-            );
-            result.east.p3.available = this.extractNumber(p3Text);
+                // 1. Detect Section based on labels present
+                items.forEach(item => {
+                    const labelEl = item.querySelector('.num-txt');
+                    if (labelEl) {
+                        const txt = labelEl.textContent;
+                        if (txt.includes('P1') || txt.includes('P3')) isEast = true;
+                        if (txt.includes('P2') || txt.includes('P4')) isWest = true;
+                    }
+                });
 
-            // 서편 (div:nth-child(5))
-            // 장기주차장 P2
-            const p2Text = this.getTextBySelector(doc,
-                `${baseSelector} > div > div:nth-child(5) > ul > li:nth-child(1) > div > div:nth-child(2) > div:first-child`
-            );
-            result.west.p2.available = this.extractNumber(p2Text);
+                // 2. Extract Data
+                items.forEach(item => {
+                    const labelEl = item.querySelector('.num-txt');
+                    const valEl = item.querySelector('.num-noti strong');
 
-            // 주차타워 서편
-            const towerWestText = this.getTextBySelector(doc,
-                `${baseSelector} > div > div:nth-child(5) > ul > li:nth-child(2) > div > div:nth-child(2) > div:first-child`
-            );
-            result.west.tower.available = this.extractNumber(towerWestText);
+                    if (labelEl && valEl) {
+                        const label = labelEl.textContent.trim();
+                        const val = this.extractNumber(valEl.textContent);
 
-            // 장기주차장 P4
-            const p4Text = this.getTextBySelector(doc,
-                `${baseSelector} > div > div:nth-child(5) > ul > li:nth-child(3) > div > div:nth-child(2) > div:first-child`
-            );
-            result.west.p4.available = this.extractNumber(p4Text);
-
-            // 대체 방법: 컨테이너에서 직접 찾기
-            if (result.east.p1.available === 0 && result.west.p2.available === 0) {
-                const container = doc.querySelector('#menu965_obj1182');
-                if (container) {
-                    const divs = container.querySelectorAll(':scope > div > div');
-
-                    divs.forEach((div, divIndex) => {
-                        const listItems = div.querySelectorAll('ul li');
-                        if (listItems.length >= 3) {
-                            listItems.forEach((li, liIndex) => {
-                                const numDiv = li.querySelector('div > div:nth-child(2) > div:first-child') ||
-                                    li.querySelector('div div:nth-of-type(2) div:first-child');
-                                if (numDiv) {
-                                    const num = this.extractNumber(numDiv.textContent);
-
-                                    // div[4] = index 3 (동편), div[5] = index 4 (서편)
-                                    if (divIndex === 3) { // 동편
-                                        if (liIndex === 0) result.east.p1.available = num;
-                                        else if (liIndex === 1) result.east.tower.available = num;
-                                        else if (liIndex === 2) result.east.p3.available = num;
-                                    } else if (divIndex === 4) { // 서편
-                                        if (liIndex === 0) result.west.p2.available = num;
-                                        else if (liIndex === 1) result.west.tower.available = num;
-                                        else if (liIndex === 2) result.west.p4.available = num;
-                                    }
-                                }
-                            });
+                        if (isEast) {
+                            if (label.includes('P1')) result.east.p1.available = val;
+                            else if (label.includes('P3')) result.east.p3.available = val;
+                            else if (label.includes('주차타워')) result.east.tower.available = val;
+                        } else if (isWest) {
+                            if (label.includes('P2')) result.west.p2.available = val;
+                            else if (label.includes('P4')) result.west.p4.available = val;
+                            else if (label.includes('주차타워')) result.west.tower.available = val;
                         }
-                    });
-                }
-            }
+                    }
+                });
+            });
 
             console.log('장기주차장 파싱 결과:', result);
-
         } catch (error) {
             console.error('장기주차장 파싱 오류:', error);
         }
